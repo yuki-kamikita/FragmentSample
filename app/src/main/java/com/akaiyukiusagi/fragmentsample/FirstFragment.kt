7f65a5324989f1b49f3981d4b583741a11318ef6
shuhei_kiyosawa <s.kiyosawa@frontierfield.co.jp>
2020-10-01 11:36:35 +0900

    buttonは全部onViewCreatedに統一 コメント追加

--- app/src/main/java/com/akaiyukiusagi/fragmentsample/FirstFragment.kt
+++ app/src/main/java/com/akaiyukiusagi/fragmentsample/FirstFragment.kt
@@ -18,19 +18,21 @@ class FirstFragmentActivity : Fragment() {
         // findViewByIdでlayoutへアクセスする //
         val view = inflater.inflate(R.layout.activity_first_fragment,container,false)
         val text: TextView = view.findViewById(R.id.firstText)
-        val button: Button = view.findViewById(R.id.buttonFinish)
         text.text = "1st text"
-
-        button.setOnClickListener {
-            fragmentManager!!.beginTransaction().remove(this).commit() // フラグメントの終了
-        }
 
         return view
     }
 
+    // onViewCreatedでならsyntheticを使用して書ける
+    // もう全部こっちで書いちゃえば基本的には問題なさそう
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         textSynthetic.text = "onViewCreatedでならsyntheticを使用して書ける"
-        // もう全部こっちで書いちゃえば基本的には問題なさそう
+
+        // フラグメントの終了 非表示、と言った方が近いかも
+        buttonFinish.setOnClickListener {
+            fragmentManager!!.beginTransaction().remove(this).commit()
+        }
+
     }
 }
